ff635306650ae1e9241b09d9d8b51eca6809f57d
Tobias Herrmann <therrmann@gmail.com>
2025-05-14 08:14:46 +0200

    deletes invalid migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   dialect: "sqlite", // 'mysql' | 'sqlite' | 'turso'
   schema: "./app/db/schema.ts",
-  out: "./app/db/drizzle",
+  out: "./app/db/migrations",
   dbCredentials: {
     url: process.env.DB_FILE_NAME!,
   },
